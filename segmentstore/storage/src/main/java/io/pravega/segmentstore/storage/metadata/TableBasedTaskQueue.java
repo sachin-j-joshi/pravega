5be2ef44cb550fc8647d47d98bcf55598378f0d4
Sachin Joshi <sachin.joshi@emc.com>
2021-06-11 16:03:13 -0700

    Fix issues Signed-off-by: Sachin Joshi <sachin.joshi@emc.com>
    Signed-off-by: Sachin Joshi <sachin.joshi@emc.com>

--- segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/metadata/TableBasedTaskQueue.java
+++ segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/metadata/TableBasedTaskQueue.java
@@ -27,6 +27,8 @@ import lombok.Builder;
 import lombok.Cleanup;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
@@ -41,8 +43,15 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * Implementation of {@link AbstractTaskQueue} using {@link TableBasedMetadataStore}.
+ */
 @Slf4j
 public class TableBasedTaskQueue implements AbstractTaskQueue<GarbageCollector.TaskInfo> {
+    public static final int CHECK_POINT_FREQUENCY = 100;
+    @Getter
+    @Setter
+    public  int checkPointFrequency = CHECK_POINT_FREQUENCY;
     private final ChunkedSegmentStorage storage;
     private final Map<String, TableBasedQueueInfo> map = Collections.synchronizedMap(new HashMap<>());
     private final int containerId;
@@ -66,11 +75,15 @@ public class TableBasedTaskQueue implements AbstractTaskQueue<GarbageCollector.T
     public void process() {
         ArrayList<GarbageCollector.TaskInfo> batch = new ArrayList<>();
         val queueName = storage.getGarbageCollector().getTaskQueueName();
+        val queueInfo = map.get(queueName);
+        if (null == queueInfo) {
+            return;
+        }
         try {
             long currentId;
             long oldId = map.get(queueName).startTaskId.get();
-            for ( currentId = oldId; currentId < 100 && currentId < map.get(queueName).endTaskId.get(); currentId++) {
-                String keyName = "TQ." + containerId + "." + queueName + "-" + currentId;
+            for (currentId = oldId; currentId < checkPointFrequency && currentId < map.get(queueName).endTaskId.get(); currentId++) {
+                String keyName = getKeyName(queueName, currentId);
                 @Cleanup
                 val txn = storage.getMetadataStore().beginTransaction(false, keyName);
                 val info = txn.get(keyName).get();
@@ -83,8 +96,8 @@ public class TableBasedTaskQueue implements AbstractTaskQueue<GarbageCollector.T
                 storage.getGarbageCollector().processBatch(batch).join();
                 map.get(queueName).startTaskId.set(currentId);
 
-                for (currentId = oldId; currentId < 100 && currentId < map.get(queueName).endTaskId.get(); currentId++) {
-                    String keyName = "TQ." + containerId + "." + queueName + "-" + currentId;
+                for (currentId = oldId; currentId < checkPointFrequency && currentId < map.get(queueName).endTaskId.get(); currentId++) {
+                    String keyName = getKeyName(queueName, currentId);
                     @Cleanup
                     val txn = storage.getMetadataStore().beginTransaction(false, keyName);
                     txn.delete(keyName);
@@ -98,29 +111,42 @@ public class TableBasedTaskQueue implements AbstractTaskQueue<GarbageCollector.T
         }
     }
 
+    private String getKeyName(String queueName, long currentId) {
+        return "TQ." + containerId + "." + queueName + "-" + currentId;
+    }
+
     /**
      * Adds a queue by the given name.
      *
-     * @param queueName Name of the queue.
+     * @param queueName        Name of the queue.
+     * @param ignoreProcessing Whether the processing should be ignored.
      */
     @Override
-    public CompletableFuture<Void> addQueue(String queueName) {
+    public CompletableFuture<Void> addQueue(String queueName, Boolean ignoreProcessing) {
         @Cleanup
         MetadataTransaction txn = storage.getMetadataStore().beginTransaction(false, queueName);
         String keyName = "TQ." + containerId + "." + queueName;
+
         return txn.get(keyName).thenComposeAsync( storageMetadata -> {
             TableBasedQueueInfo info = (TableBasedQueueInfo) storageMetadata;
-            if (null == storageMetadata) {
+            if (null == info) {
+                log.info("TableBasedTaskQueue[{}]: initializing info for queue={}", containerId, keyName);
                 info = TableBasedQueueInfo.builder()
                         .name(keyName)
+                        .ignoreProcessing(ignoreProcessing)
+                        .startTaskId(new AtomicLong())
+                        .endTaskId(new AtomicLong())
+                        .lastBatch(new AtomicLong())
                         .build();
                 map.put(queueName, info);
                 txn.create(info);
                 return txn.commit();
 
             } else {
+                log.info("TableBasedTaskQueue[{}]: loading info for queue={} info={}", containerId, keyName, info);
+                info.endTaskId.set(info.lastBatch.get() + checkPointFrequency);
                 map.put(queueName, info);
-                return CompletableFuture.completedFuture(null);
+                return saveQueueState(queueName, info);
             }
         });
     }
@@ -136,13 +162,10 @@ public class TableBasedTaskQueue implements AbstractTaskQueue<GarbageCollector.T
         val q = map.get(queueName);
         val id = q.endTaskId.incrementAndGet();
         CompletableFuture<Void> f = null;
-        if (id - q.lastBatch.get() > 100) {
+        if (id - q.lastBatch.get() > checkPointFrequency) {
             synchronized (q) {
-                if (id - q.lastBatch.get() > 100) {
-                    try (val txn = storage.getMetadataStore().beginTransaction(false, queueName)) {
-                        txn.update(q);
-                        f = txn.commit();
-                    }
+                if (id - q.lastBatch.get() > checkPointFrequency) {
+                    f = saveQueueState(queueName, q);
                 }
             }
         }
@@ -163,6 +186,13 @@ public class TableBasedTaskQueue implements AbstractTaskQueue<GarbageCollector.T
         }
     }
 
+    private CompletableFuture<Void> saveQueueState(String queueName, TableBasedQueueInfo info) {
+        try (val txn = storage.getMetadataStore().beginTransaction(false, queueName)) {
+            txn.update(info);
+            return txn.commit();
+        }
+    }
+
     @Override
     public void close() throws Exception {
         processorTask.cancel(true);
@@ -173,9 +203,10 @@ public class TableBasedTaskQueue implements AbstractTaskQueue<GarbageCollector.T
     @EqualsAndHashCode(callSuper = true)
     static public class TableBasedQueueInfo extends StorageMetadata {
         private String name;
-        private final AtomicLong startTaskId = new AtomicLong();
-        private final AtomicLong endTaskId = new AtomicLong();
-        private final AtomicLong lastBatch = new AtomicLong();
+        private boolean ignoreProcessing;
+        private AtomicLong startTaskId;
+        private AtomicLong endTaskId;
+        private AtomicLong lastBatch;
         /**
          * Retrieves the key associated with the metadata.
          *
@@ -193,9 +224,13 @@ public class TableBasedTaskQueue implements AbstractTaskQueue<GarbageCollector.T
          */
         @Override
         public TableBasedQueueInfo deepCopy() {
-            val retVal =  new TableBasedQueueInfoBuilder().build();
-            retVal.startTaskId.set(this.startTaskId.get());
-            retVal.endTaskId.set(this.endTaskId.get());
+            val retVal =  new TableBasedQueueInfoBuilder()
+                    .name(this.name)
+                    .ignoreProcessing(this.ignoreProcessing)
+                    .startTaskId(new AtomicLong(this.startTaskId.get()))
+                    .endTaskId(new AtomicLong(this.endTaskId.get()))
+                    .lastBatch(new AtomicLong(this.lastBatch.get()))
+                    .build();
             return retVal;
         }
 
@@ -226,11 +261,18 @@ public class TableBasedTaskQueue implements AbstractTaskQueue<GarbageCollector.T
 
             private void write00(TableBasedQueueInfo object, RevisionDataOutput output) throws IOException {
                 output.writeUTF(object.name);
-
+                output.writeBoolean(object.ignoreProcessing);
+                output.writeCompactLong(object.startTaskId.get());
+                output.writeCompactLong(object.endTaskId.get());
+                output.writeCompactLong(object.lastBatch.get());
             }
 
             private void read00(RevisionDataInput input, TableBasedQueueInfo.TableBasedQueueInfoBuilder b) throws IOException {
                 b.name(input.readUTF());
+                b.ignoreProcessing(input.readBoolean());
+                b.startTaskId(new AtomicLong(input.readCompactLong()));
+                b.endTaskId(new AtomicLong(input.readCompactLong()));
+                b.lastBatch(new AtomicLong(input.readCompactLong()));
             }
         }
     }
